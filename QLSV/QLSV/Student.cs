f078cfbaedfc50b94abe20ae3b04f4b076f994c8
Ket <kiet207092005@gmail.com>
2025-11-05 17:53:07 +0700

    Thêm mã nguồn thư mục QLSV lên nhánh user2, loại trừ sln

---
+++ QLSV/QLSV/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    internal class Student
+    {
+        public string MaSV { get; set; }
+        public string HoTen { get; set; }
+        public string Lop { get; set; }
+        public double Diem { get; set; }
+
+        public Student(string maSV, string hoTen, string lop, double diem)
+        {
+            MaSV = maSV;
+            HoTen = hoTen;
+            Lop = lop;
+            Diem = diem;
+        }
+
+    }
+}
---
+++ QLSV/QLSV/StudentManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    internal class StudentManager
+    {
+        private List<Student> danhSach = new List<Student>();
+
+        public List<Student> LayDanhSach() => danhSach;
+
+        public void Them(Student sv)
+        {
+            danhSach.Add(sv);
+        }
+
+        public void Xoa(string maSV)
+        {
+            var sv = danhSach.FirstOrDefault(s => s.MaSV == maSV);
+            if (sv != null)
+                danhSach.Remove(sv);
+        }
+
+        public void Sua(Student svMoi)
+        {
+            var sv = danhSach.FirstOrDefault(s => s.MaSV == svMoi.MaSV);
+            if (sv != null)
+            {
+                sv.HoTen = svMoi.HoTen;
+                sv.Lop = svMoi.Lop;
+                sv.Diem = svMoi.Diem;
+            }
+        }
+
+        public List<Student> TimKiem(string keyword)
+        {
+            return danhSach
+                .Where(s => s.HoTen.ToLower().Contains(keyword.ToLower()) || s.MaSV.Contains(keyword))
+                .ToList();
+        }
+    }
+}
